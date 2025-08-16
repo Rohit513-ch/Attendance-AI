6bee9bfa5b5e3e5047a0191ae085c5744631070d
Rohit Chandra <rohitchandrapandit@gmail.com>
2025-08-16 14:44:06 +0000

    allign all the pages and their contents properly

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -114,8 +114,8 @@ export default function DashboardLayout({
                 AttendAI
              </div>
             </header>
-            <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-            {children}
+            <main className="flex-1">
+                {children}
             </main>
         </div>
       </div>
