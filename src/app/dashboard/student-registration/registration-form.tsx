df35fa3bd18dd66a8b077aef0b6199fb3370dd5e
Rohit Chandra <rohitchandrapandit@gmail.com>
2025-08-16 14:38:27 +0000

    add another option of "others" under Gender in Student Registration Form

--- src/app/dashboard/student-registration/registration-form.tsx
+++ src/app/dashboard/student-registration/registration-form.tsx
@@ -91,6 +91,10 @@ export function RegistrationForm() {
                     <RadioGroupItem value="female" id="female" />
                     <Label htmlFor="female">Female</Label>
                   </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="other" id="other" />
+                    <Label htmlFor="other">Other</Label>
+                  </div>
                 </RadioGroup>
               </div>
               <div>
