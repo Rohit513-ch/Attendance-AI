62b4c1e7c977a5e9ea0584caa0881c256e5cf5ff
Rohit Chandra <rohitchandrapandit@gmail.com>
2025-08-16 15:57:29 +0000

    also make them yellow

--- src/app/dashboard/attendance-details/page.tsx
+++ src/app/dashboard/attendance-details/page.tsx
@@ -324,7 +324,7 @@ export default function AttendanceDetailsPage() {
                           !date && "text-muted-foreground"
                         )}
                       >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
+                        <CalendarIcon className="mr-2 h-4 w-4 text-yellow-400" />
                         {date ? (
                           format(date, "LLL dd, y")
                         ) : (
@@ -461,3 +461,4 @@ export default function AttendanceDetailsPage() {
       </div>
     </div>
   );
+}
